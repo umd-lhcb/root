badcb1515a17b178c7e5f2b577b90f06745ff7a7
Yipeng Sun <syp@umd.edu>
2022-11-30 15:27:35 -0500

    Use ROOT-5 specific build patches.

--- nix/default.nix
+++ nix/default.nix
@@ -1,100 +1,86 @@
 { stdenv
 , lib
-, fetchurl
-, makeWrapper
+, fetchFromGitHub
 , cmake
-, ftgl
-, gl2ps
-, glew
-, gsl
+, pcre
+, pkgconfig
 , libX11
 , libXpm
 , libXft
 , libXext
 , libGLU
 , libGL
-, libxml2
-, expat
-, lz4
-, xz
-, pcre
-, pkg-config
-, python
-, xxHash
 , zlib
-, zstd
-, libAfterImage
-, giflib
-, libjpeg
-, libtiff
-, libpng
+, libxml2
+, lz4
+, lzma
+, gsl_1
+, xxHash
 , Cocoa
-, CoreSymbolication
 , OpenGL
 , noSplash ? false
 }:
 
 stdenv.mkDerivation rec {
   pname = "root";
-  version = "6.16.00";
-  src = ./..;
+  version = "5.34.38";
 
-  nativeBuildInputs = [ makeWrapper cmake pkg-config ];
-  buildInputs = [ ftgl gl2ps glew pcre zlib zstd libxml2 lz4 xz gsl xxHash libAfterImage giflib libjpeg libtiff libpng python.pkgs.numpy ]
-    ++ lib.optionals (!stdenv.isDarwin) [ libX11 libXpm libXft libXext libGLU libGL expat ]
-    ++ lib.optionals (stdenv.isDarwin) [ Cocoa CoreSymbolication OpenGL ]
+  src = ../.;
+
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ cmake pcre zlib libxml2 lz4 lzma gsl_1 xxHash ]
+    ++ lib.optionals (!stdenv.isDarwin) [ libX11 libXpm libXft libXext libGLU libGL ]
+    ++ lib.optionals (stdenv.isDarwin) [ Cocoa OpenGL ]
   ;
 
   patches = [
-    ./sw_vers.patch
-    ./fix_root_config.patch
-    ./fix_histo_same_binning.patch
+    # various fixes to ROOT build system
+    ./sw_vers_root5.patch
+
+    # prevents rootcint from looking in /usr/includes and such
+    ./purify_include_paths_root5.patch
+
+    # disable dictionary generation for stuff that includes libc headers
+    # our glibc requires a modern compiler
+    ./disable_libc_dicts_root5.patch
+
+    ./hist_factory.patch
   ];
 
   preConfigure = ''
-    rm -rf builtins/*
-    substituteInPlace cmake/modules/SearchInstalledSoftware.cmake \
-      --replace 'set(lcgpackages ' '#set(lcgpackages '
-
-    # Don't require textutil on macOS
-    : > cmake/modules/RootCPack.cmake
-
     patchShebangs build/unix/
+    ln -s ${lib.getDev stdenv.cc.libc}/include/AvailabilityMacros.h cint/cint/include/
+  ''
+  # Fix CINTSYSDIR for "build" version of rootcint
+  # This is probably a bug that breaks out-of-source builds
+  + ''
+    substituteInPlace cint/cint/src/loadfile.cxx\
+      --replace 'env = "cint";' 'env = "'`pwd`'/cint";'
   '' + lib.optionalString noSplash ''
     substituteInPlace rootx/src/rootx.cxx --replace "gNoLogo = false" "gNoLogo = true"
-  '' + lib.optionalString stdenv.isDarwin ''
-    # Eliminate impure reference to /System/Library/PrivateFrameworks
-    substituteInPlace core/CMakeLists.txt \
-      --replace "-F/System/Library/PrivateFrameworks" ""
   '';
 
   cmakeFlags = [
     "-Drpath=ON"
-    "-DCMAKE_CXX_STANDARD=17"
     "-DCMAKE_INSTALL_LIBDIR=lib"
     "-DCMAKE_INSTALL_INCLUDEDIR=include"
-    "-Dbuiltin_afterimage=OFF"
-    "-Dbuiltin_ftgl:BOOL=OFF"
-    "-Dbuiltin_glew:BOOL=ON"
-    "-Dbuiltin_unuran=OFF"
     "-Dalien=OFF"
     "-Dbonjour=OFF"
     "-Dcastor=OFF"
     "-Dchirp=OFF"
-    "-Dclad=OFF"
-    "-Dcxx17=ON"
+    "-Dcxx11=ON"
     "-Ddavix=OFF"
     "-Ddcache=OFF"
-    "-Dfail-on-missing=ON"
     "-Dfftw3=OFF"
     "-Dfitsio=OFF"
     "-Dfortran=OFF"
-    "-Dimt=OFF"
     "-Dgfal=OFF"
+    "-Dgsl_shared=ON"
     "-Dgviz=OFF"
     "-Dhdfs=OFF"
     "-Dkrb5=OFF"
     "-Dldap=OFF"
+    "-Dmathmore=ON"
     "-Dmonalisa=OFF"
     "-Dmysql=OFF"
     "-Dodbc=OFF"
@@ -103,31 +89,17 @@ stdenv.mkDerivation rec {
     "-Dpgsql=OFF"
     "-Dpythia6=OFF"
     "-Dpythia8=OFF"
+    "-Dpython=OFF"
     "-Drfio=OFF"
-    "-Droot7=OFF"
+    "-Droofit=ON"
     "-Dsqlite=OFF"
     "-Dssl=OFF"
-    "-Dunuran:BOOL=OFF"
-    "-Dvdt=OFF"
     "-Dxml=ON"
     "-Dxrootd=OFF"
   ]
-  ++ lib.optional (stdenv.cc.libc != null) "-DC_INCLUDE_DIRS=${lib.getDev stdenv.cc.libc}/include"
-  ++ lib.optionals stdenv.isDarwin [
-    "-DOPENGL_INCLUDE_DIR=${OpenGL}/Library/Frameworks"
-    #"-DCMAKE_DISABLE_FIND_PACKAGE_Python2=TRUE"
+  ++ lib.optional stdenv.isDarwin "-DOPENGL_INCLUDE_DIR=${OpenGL}/Library/Frameworks";
 
-    # fatal error: module map file '/nix/store/<hash>-Libsystem-osx-10.12.6/include/module.modulemap' not found
-    # fatal error: could not build module '_Builtin_intrinsics'
-    "-Druntime_cxxmodules=OFF"
-  ];
-
-  postInstall = ''
-    for prog in rootbrowse rootcp rooteventselector rootls rootmkdir rootmv rootprint rootrm rootslimtree; do
-      wrapProgram "$out/bin/$prog" \
-        --prefix PYTHONPATH : "$out/lib"
-    done
-  '';
+  enableParallelBuilding = true;
 
   setupHook = ./setup-hook.sh;
 
@@ -135,7 +107,7 @@ stdenv.mkDerivation rec {
     homepage = "https://root.cern.ch/";
     description = "A data analysis framework";
     platforms = platforms.unix;
-    maintainers = [ maintainers.veprbl ];
+    # maintainers = with maintainers; [ veprbl ];  # This is a local port, and is not maintained by Dmitry!
     license = licenses.lgpl21;
   };
 }
